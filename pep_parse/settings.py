077f8294882020d0401aaa4fcbf44a789d4545ee
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-10-13 18:43:42 +0300

    fix pep8

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -9,7 +9,7 @@ ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
     'pep_parse.pipelines.PepParsePipeline': 300,
-    }
+}
 
 FEEDS = {
     'results/pep_%(time)s.csv': {
@@ -17,41 +17,4 @@ FEEDS = {
         'fields': ['number', 'name', 'status'],
         'overwrite': True
     },
-} 
-
-#USER_AGENT = 'pep_parse (+http://www.yourdomain.com)'
-#CONCURRENT_REQUESTS = 32
-#DOWNLOAD_DELAY = 3
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-#COOKIES_ENABLED = False
-#TELNETCONSOLE_ENABLED = False
-
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-#SPIDER_MIDDLEWARES = {
-#    'pep_parse.middlewares.PepParseSpiderMiddleware': 543,
-#}
-
-#DOWNLOADER_MIDDLEWARES = {
-#    'pep_parse.middlewares.PepParseDownloaderMiddleware': 543,
-#}
-
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-#AUTOTHROTTLE_ENABLED = True
-#AUTOTHROTTLE_START_DELAY = 5
-#AUTOTHROTTLE_MAX_DELAY = 60
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-#AUTOTHROTTLE_DEBUG = False
-
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+}
